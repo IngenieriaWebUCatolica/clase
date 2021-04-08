d425ba1f9ee9ba6b49eadd17b61a3082b6adc1e7
Jaime Zamora <kratov@protonmail.com>
2021-04-08 15:02:44 -0500

    Added XMLHttpRequest

---
+++ index.js
@@ -0,0 +1,26 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+const get = (URL, response) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        const DONE = 4;
+        const OK = 200;
+        if (xhr.readyState === DONE) {
+            if (xhr.status === OK) {
+                response(null, JSON.parse(xhr.responseText))
+            } else {
+                response(new Error('Se produjo un error al realizar el request'), JSON.parse(xhr.responseText))
+            }
+        }
+    }
+
+    xhr.open('GET', URL);
+    xhr.send(null);
+}
+
+
+get('https://swapi.dev/api/planets/20/', (err, response) => {
+    if (err) return console.error(`Request failed ${err}`);
+    console.log('Request succeeded');
+    console.log('Data', response);
+});
